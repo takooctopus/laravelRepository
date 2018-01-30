a2204805e5954ba9a6c9b399f92446818b9572d2
takooctopus <takooctopus@hotmail.com>
2018-01-30 14:14:46 +0800

    init

---
+++ src/Exceptions/RepositoryException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tako
+ * Date: 2018/1/30
+ * Time: 12:44
+ */
+namespace Takooctopus\Repositories\Exceptions;
+
+use Exception;
+
+class RepositoryException extends Exception
+{
+
+}
